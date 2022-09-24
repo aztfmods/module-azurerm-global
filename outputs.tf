41fe47b561b60c2af357b37eebd9064b00d5910e
dkool <dennis.kool@cloudnation.nl>
2022-09-24 09:11:16 +0200

    outputs list of maps

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,17 @@
+# output "resourcegroups" {
+#   value = azurerm_resource_group.rg
+# }
+
 output "resourcegroups" {
-  value = azurerm_resource_group.rg
+  value = [
+    for entry in azurerm_resource_group.rg:
+    entry
+  ]
 }
+
+# output "private_addresses_new" {
+#   value = [
+#     for instance in aws_instance.ubuntu:
+#     instance.private_dns
+#   ]
+# }

4737f0f43a1e78307e4341cb655bf2866a873117
dkool <dennis.kool@cloudnation.nl>
2022-12-06 08:46:16 +0100

    cleanup

--- outputs.tf
+++ outputs.tf
@@ -13,25 +13,3 @@ output "env" {
 output "region" {
   value = local.region
 }
-
-
-
-# output "foo" {
-#   value = local.optional_input / 5
-# }
-
-# variable "optional_input" {
-#   description = "An optional number... If not specified, the default is 5"
-#   type        = number
-#   default     = null
-# }
-
-# locals {
-#   # If var.optional_input isn't null, use var.optional_input; otherwise, use 5
-#   # as the default
-#   optional_input = var.optional_input != null ? var.optional_input : 5
-# }
-
-# output "foo" {
-#   value = local.optional_input / 5
-# }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,16 @@
 variable "rgs" {}
 
 variable "company" {
-  description = "An optional number... If not specified, the default is 5"
-  type        = string
-  default     = null
+  type    = string
+  default = null
 }
 
 variable "region" {
-  description = "An optional number... If not specified, the default is 5"
-  type        = string
-  default     = null
+  type    = string
+  default = null
 }
 
 variable "env" {
-  description = "An optional number... If not specified, the default is 5"
-  type        = string
-  default     = null
+  type    = string
+  default = null
 }
